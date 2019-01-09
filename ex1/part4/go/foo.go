f20bac2e72cb0ccf831d709f34c2d207ed897cc7
Einar <einar@omang.com>
2019-01-09 12:03:44 +0100

    Part 4 Go code

---
+++ ex1/part4/go/foo.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    . "fmt"
+    "runtime"
+    "time"
+)
+
+var i = 0
+
+func incrementing() {
+    for j := 0; j < 1000000; j++ {
+        i++
+    }
+}
+
+func decrementing() {
+    for j := 0; j < 1000000; j++ {
+        i--
+    }
+}
+
+func main() {
+    runtime.GOMAXPROCS(runtime.NumCPU() - 1)    // I guess this is a hint to what GOMAXPROCS does...
+    go incrementing()
+    go decrementing()
+    // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
+    // We'll come back to using channels in Exercise 2. For now: Sleep.
+    time.Sleep(100*time.Millisecond)
+    Println("The magic number is:", i)
+}
